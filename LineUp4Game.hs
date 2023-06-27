197f7bbdaa82068747ef2649db7008f690292d8f
Ari-J-B <ariboydit@gmail.com>
2023-06-28 00:11:03 +1000

    Defined game state
    The state is what the AI will be interacting with

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -3,3 +3,7 @@ data Piece = Yellow | Red | Empty
 type Row = (Piece, Piece, Piece, Piece, Piece, Piece, Piece)
 
 type Board = (Row, Row, Row, Row, Row, Row)
+
+data Turn = Player1 | Player2
+
+type GameState = (Board, Turn)
