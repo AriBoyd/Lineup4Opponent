cbda5a3c2a19e2890d244b7ac0fcd8eb24c2c6ca
Ari-J-B <ariboydit@gmail.com>
2023-06-28 00:50:12 +1000

    Added function to swap players

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -13,6 +13,10 @@ type Move = Int
 -- The game is entirely defined by the board and whose turn it is
 type GameState = (Board, Player)
 
+swapPlayer :: Player -> Player
+swapPlayer Red = Yellow
+swapPlayer Yellow = Red
+
 placeTokenColumn :: Column -> Player -> Column
 placeTokenColumn [] _ = error "Empty Column"
 placeTokenColumn (Nothing : Nothing : rest) player = Nothing : placeTokenColumn (Nothing : rest) player -- Descend a row
@@ -20,6 +24,6 @@ placeTokenColumn (Nothing : rest) player = Just player : rest -- Found the spot
 placeTokenColumn _ _ = error "No Free Space"
 
 nextState :: GameState -> Move -> GameState
-nextState state move = placeToken (board !! move) player
+nextState state move = placeToken board move player swapPlayer player
   where
     (board, player) = state
