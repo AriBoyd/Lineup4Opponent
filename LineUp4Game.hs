66808916b9ff4be415a3bc32884f6993749db998
Ari-J-B <ariboydit@gmail.com>
2023-06-28 00:29:38 +1000

    Transposed the board, scaffolded the next state function

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -2,8 +2,15 @@ data Player = Red | Yellow
 
 type Piece = Maybe Player
 
-type Row = (Piece, Piece, Piece, Piece, Piece, Piece, Piece)
+type Column = (Piece, Piece, Piece, Piece, Piece, Piece)
 
-type Board = (Row, Row, Row, Row, Row, Row)
+type Board = (Column, Column, Column, Column, Column, Column, Column)
+
+data Move = Zero | One | Two | Three | Four | Five | Six deriving (Read, Show, Enum, Eq, Ord)
 
 type GameState = (Board, Player)
+
+nextState :: GameState -> Move -> GameState
+nextState state move = placePiece (board !! move) player
+  where
+    (board, player) = state
