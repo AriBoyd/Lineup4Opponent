23643db2acb21ddeb7c12322e03562b70ef8f8ac
Ari-J-B <ariboydit@gmail.com>
2023-06-28 00:13:56 +1000

    Rewrote definition for the pieces so it will be more consistent

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -1,9 +1,9 @@
-data Piece = Yellow | Red | Empty
+data Player = Red | Yellow
+
+type Piece = Maybe Player
 
 type Row = (Piece, Piece, Piece, Piece, Piece, Piece, Piece)
 
 type Board = (Row, Row, Row, Row, Row, Row)
 
-data Turn = Player1 | Player2
-
-type GameState = (Board, Turn)
+type GameState = (Board, Player)
