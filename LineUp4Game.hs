fc98cd120ff75d055e6bc7e144479694ce3d82c5
Ari-J-B <ariboydit@gmail.com>
2023-06-28 00:48:09 +1000

    Added function to place a token in a column

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -1,16 +1,25 @@
+-- There are two players, Red and Yellow
 data Player = Red | Yellow
 
-type Piece = Maybe Player
+-- A spot can have either player's token, or be empty
+type Spot = Maybe Player
 
-type Column = (Piece, Piece, Piece, Piece, Piece, Piece)
+type Column = [Spot]
 
-type Board = (Column, Column, Column, Column, Column, Column, Column)
+type Board = [Column]
 
-data Move = Zero | One | Two | Three | Four | Five | Six deriving (Read, Show, Enum, Eq, Ord)
+type Move = Int
 
+-- The game is entirely defined by the board and whose turn it is
 type GameState = (Board, Player)
 
+placeTokenColumn :: Column -> Player -> Column
+placeTokenColumn [] _ = error "Empty Column"
+placeTokenColumn (Nothing : Nothing : rest) player = Nothing : placeTokenColumn (Nothing : rest) player -- Descend a row
+placeTokenColumn (Nothing : rest) player = Just player : rest -- Found the spot
+placeTokenColumn _ _ = error "No Free Space"
+
 nextState :: GameState -> Move -> GameState
-nextState state move = placePiece (board !! move) player
+nextState state move = placeToken (board !! move) player
   where
     (board, player) = state
