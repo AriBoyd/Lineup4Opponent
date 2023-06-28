af62644b9b40403139bf0d7f6f00977281109582
Ari-J-B <ariboydit@gmail.com>
2023-06-28 16:16:23 +1000

    Added function to get the valid moves for a state, renamed spot to space

--- LineUp4Game.hs
+++ LineUp4Game.hs
@@ -1,10 +1,12 @@
+import Data.List (findIndices)
+
 -- There are two players, Red and Yellow
 data Player = Red | Yellow
 
--- A spot can have either player's token, or be empty
-type Spot = Maybe Player
+-- A space can have either player's token, or be empty
+type Space = Maybe Player
 
-type Column = [Spot]
+type Column = [Space] -- 0th index is considered the top row, 6th is the bottom
 
 type Board = [Column]
 
@@ -20,7 +22,7 @@ swapPlayer Yellow = Red
 placeTokenColumn :: Column -> Player -> Column
 placeTokenColumn [] _ = error "Empty Column"
 placeTokenColumn (Nothing : Nothing : rest) player = Nothing : placeTokenColumn (Nothing : rest) player -- Descend a row
-placeTokenColumn (Nothing : rest) player = Just player : rest -- Found the spot
+placeTokenColumn (Nothing : rest) player = Just player : rest -- Found the lowest empty space
 placeTokenColumn _ _ = error "No Free Space"
 
 placeToken :: Board -> Move -> Player -> Board
@@ -30,3 +32,10 @@ nextState :: GameState -> Move -> GameState
 nextState state move = (placeToken board move player, swapPlayer player)
   where
     (board, player) = state
+
+freeSpace :: Column -> Bool
+freeSpace (Nothing : _) = True -- Since pieces fall, any non-full column has an empty space at the top
+freeSpace _ = False
+
+getValidMoves :: GameState -> [Move]
+getValidMoves (board, _) = findIndices freeSpace board
